6f1e477cf74c664474829bedffadf68152021d80
carban <murillo.carlos@correounivalle.edu.co>
2024-11-06 20:58:08 -0500

    foot

--- next-app/src/app/components/foot.tsx
+++ next-app/src/app/components/foot.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Foot() {
     return <div className="w-full h-10 flex items-center justify-around bg-gray-900">
         <Link href="/">
-            <p className="text-lg font-semibold text-primary">f4k3
-                <b>81t5.10</b>
+            <p className="text-lg font-semibold text-primary">
+                <b>F004P1.C0M</b>
             </p>
         </Link>
     </div>
