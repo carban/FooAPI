5d18f8db2e7947baa6d9e9dffe2e3d83a75411f2
Carlos Esteban Murillo S. <42922050+carban@users.noreply.github.com>
2024-11-14 09:33:36 -0500

    Update layout

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -7,11 +7,11 @@ const inter = Inter({ weight: ["400"], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
-    default: "FooApi",
+    default: "Foo Api",
     template: "FooApi - %s"
   },
-  description: 'The All in one Fake Api - Dummy data for your projects,  fast and simple. Users,  products,  posts,  comments and more',
-  keywords: "API, dummy API, fake data, mock data, test data, API services, REST API, GraphQL API, GeoJSON API, API documentation, developer tools, FooApi, dummy data API, fake data API, mock data API, test data API, API for developers, REST API services, GraphQL API services, GeoJSON API services, API documentation, developer tools, API data, API GraphQL, API GeoJSON, user data API, todo data API, product data API, post data API, comment data API, image data API, movie data API, song data API, country data API, city data API"
+  description: 'The All in one Fake API - Dummy data for your projects, fast and simple. Users, products, posts, comments and more',
+  keywords: "API, dummy API, fake data, fake API, foo API, mock data, test data, API services, REST API, GraphQL API, GeoJSON API, API documentation, developer tools, FooApi, dummy data API, fake data API, mock data API, test data API, API for developers, REST API services, GraphQL API services, GeoJSON API services, API documentation, developer tools, API data, API GraphQL, API GeoJSON, user data API, todo data API, product data API, post data API, comment data API, image data API, movie data API, song data API, country data API, city data API"
 }
 
 export default function RootLayout({
