330fa1f3fa4e670c06a7aa5ea4cadfe6f30009dd
carban <murillo.carlos@correounivalle.edu.co>
2024-11-20 12:10:27 -0500

    meta

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -8,10 +8,10 @@ const inter = Inter({ weight: ["400"], subsets: ['latin'] })
 export const metadata: Metadata = {
   title: {
     default: "FooApi | The All in one Fake API",
-    template: "FooApi - %s"
+    template: "%s | FooApi"
   },
   description: 'The All in one Fake API - Dummy data for your projects, fast and simple. Users, products, posts, comments and more',
-  keywords: "API, dummy API, fake data, fake API, foo API, fooapi, foo api, fooApi, fooAPI, mock data, test data, API services, REST API, GraphQL API, GeoJSON API, API documentation, developer tools, FooApi, dummy data API, fake data API, mock data API, test data API, API for developers, REST API services, GraphQL API services, GeoJSON API services, API documentation, developer tools, API data, API GraphQL, API GeoJSON, user data API, todo data API, product data API, post data API, comment data API, image data API, movie data API, song data API, country data API, city data API, dummy json, dummyJSON"
+  keywords: "foo API, fooApi, dummy API, fake data, fake API, fake api, fakeapi, fooapi, foo api, foo API, foo API, foo api, dummy fake api, fooAPI, mock data, test data, API services, REST API, GraphQL API, GeoJSON API, FooApi, comments fake api, posts fake api, products fake api, users fake api, todos fake api, songs fake api, movies fake api, countries fake api, cities fake api, images fake api, dummy data API, fake data API, mock data API, test data API, API for developers, REST API services, GraphQL API services, GeoJSON API services, API data, API GraphQL, API GeoJSON, user data API, todo data API, product data API, post data API, comment data API, image data API, movie data API, song data API, country data API, city data API, dummy json, dummyJSON"
 }
 
 export default function RootLayout({
